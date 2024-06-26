4239709c8fff9cd52e8929c5473175c4818a4dae
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2024-06-26 09:38:39 +0600

    tailwind css added

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    {{ $slot }}
+    <div>
+        <div>
+            {{ $slot }}
+        </div>
+    </div>
 </body>
 </html>
