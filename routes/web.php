0477f5e9d23aa811db383c001d64fede1acbbb7d
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2024-06-26 09:05:47 +0600

    login Architecture done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/login', [LoginController::class, 'index'])->name('auth.login');
+Route::post('/auth/login', [LoginController::class, 'store']);
